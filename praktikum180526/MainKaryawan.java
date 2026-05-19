6782015733c6525be884d4e67ba89bdb4714f486
daffahardiano652-svg <daffahardiano652@gmail.com>
2026-05-19 17:34:17 +0700

    Implement employee status selection and salary calculation
    Added functionality to select employee status and calculate salary accordingly.

--- praktikum180526/MainKaryawan.java
+++ praktikum180526/MainKaryawan.java
@@ -16,18 +16,37 @@ public class MainKaryawan {
         Scanner input = new Scanner(System.in);
 
         System.out.println("=== SISTEM PENGGAJIAN KARYAWAN ===");
-        
-        System.out.print("Masukkan nama karyawan       : ");
+        System.out.print("Masukkan nama karyawan : ");
         String nama = input.nextLine();
-        
-        System.out.print("Masukkan gaji pokok bulanan  : Rp");
-        double gaji = input.nextDouble();
 
-        KaryawanTetap pegawai = new KaryawanTetap(nama, gaji);
+        System.out.println("\nPilih Status Karyawan:");
+        System.out.println("1. Karyawan Tetap");
+        System.out.println("2. Karyawan Kontrak");
+        System.out.print("Masukkan pilihan (1/2) : ");
+        int pilihan = input.nextInt();
 
-        System.out.println("\n--- SLIP GAJI ---");
-        System.out.println("Nama Karyawan : " + pegawai.getNama());
-        System.out.println("Total Gaji    : Rp" + pegawai.hitungTotalGaji() + " (Termasuk Tunjangan 2.5%)");
+        Karyawan pegawai = null; 
+        if (pilihan == 1) {
+            System.out.print("Masukkan gaji pokok bulanan : Rp");
+            double gaji = input.nextDouble();
+            pegawai = new KaryawanTetap(nama, gaji); 
+            
+        } else if (pilihan == 2) {
+            System.out.print("Masukkan upah per jam       : Rp");
+            double upah = input.nextDouble();
+            
+            System.out.print("Masukkan jumlah jam kerja   : ");
+            int jam = input.nextInt();
+            pegawai = new KaryawanKontrak(nama, upah, jam);
+            
+        } else {
+            System.out.println("Pilihan tidak valid!");
+        }
+
+        if (pegawai != null) {
+            System.out.println("\n--- SLIP GAJI ---");
+            pegawai.cetakGaji(); 
+        }
 
         input.close();
     }
