754fbf62faf6fe5ad99f35c543b6aaf63171f502
daffahardiano652-svg <daffahardiano652@gmail.com>
2026-05-19 10:28:00 +0700

    Add Karyawan class with basic structure

---
+++ praktikum180526/Karyawan.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package praktikum180526;
+
+/**
+ *
+ * @author acer
+ */
+public class Karyawan {
+    private String nama;
+
+    public Karyawan(String nama) {
+        this.nama = nama;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public double hitungTotalGaji() {
+        return 0; // Default
+    }
+}
