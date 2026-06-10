274d0564a97f2b01904e10066c1e1f45fcbb9d3d
daffahardiano652-svg <daffahardiano652@gmail.com>
2026-06-10 22:22:03 +0700

    Add Kendaraan class with properties and methods

---
+++ Tugaas Minggu ke 9/Kendaraan.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package praktikum27042026;
+
+/**
+ *
+ * @author acer
+ */
+public class Kendaraan {
+    String nama;
+    private int tahunProduksi;
+    
+    public String getNama(){
+        return nama;
+    }
+        
+    public void setNama(String nama){
+        this.nama = nama;
+    }
+    
+    public int getTahunProduksi(){
+        return tahunProduksi;
+    }
+    
+    public void setTahunProduksi(int tahunproduksi){
+        this.tahunProduksi = tahunProduksi;
+    }
+    
+    public Kendaraan(){
+    
+    }
+    
+    public Kendaraan(String nama, int tahunProduksi){
+        this.nama = nama;
+        this.tahunProduksi = tahunProduksi;
+    }
+    
+    public void cetak(){
+        System.out.println("Nama Kendarran : " + nama);
+        System.out.println("Tahun Produksi : " + tahunProduksi);
+    }
+  }
